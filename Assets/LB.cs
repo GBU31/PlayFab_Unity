ef67c985b771f85d9c79166f2ed435e4abd72822
brookehorizon <danieltashman17@gmail.com>
2022-08-16 21:38:02 +0300

    Updated

--- Assets/LB.cs
+++ Assets/LB.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
+using UnityEngine.SceneManagement;
 using PlayFab.ClientModels;
 
 public class LB : MonoBehaviour
 {
+    public void GetMatchScene(){
+        SceneManager.LoadScene(2);
+
+    }
+
     public void SubmitScore(int playerScore) {
     PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest {
         Statistics = new List<StatisticUpdate> {
